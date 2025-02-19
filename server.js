3ba7087c60175e6de4e5a93a375ef22061a57578
Sanket Jaswal <sanket.jaswal123@gmail.com>
2025-02-19 22:08:41 +0530

    final commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path"; 
+import { fileURLToPath } from "url"; 
 
 import bookRoutes from "./routes/bookRoutes.js";
 import recommendationRoutes from "./routes/recommendationRoutes.js"; 
@@ -21,9 +23,16 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public"))); 
+
 app.use("/api/books", bookRoutes); 
 app.use("/api/recommendations", recommendationRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
